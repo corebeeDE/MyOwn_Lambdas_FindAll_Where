aed5f76c94cdf000df6839ef832dade9ae8d8967
Korbinian Offensperger <korbinian_offensperger@yahoo.de>
2022-02-23 09:04:22 +0100

    Projektdateien hinzufügen.

---
+++ MyOwn_Lambdas_FindAll_Where/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwn_Lambdas_FindAll_Where
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> startList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Find all even numbers - first solution
+            // List<int> evenList = startList.FindAll(delegate (int x) { return x % 2 == 0; });
+
+            // Find all even numbers - better solution
+            List<int> evenList = startList.FindAll((int x) => x % 2 == 0);
+
+            // Find all odd numbers - another solution
+            List<int> oddList = startList.Where(x => x % 2 == 0).ToList();
+
+            Console.WriteLine("evenList is:");
+            foreach (var item in evenList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("oddList is:");
+            oddList.ForEach(x => Console.WriteLine(x)); // another approach to loop through each element of that list.
+
+            Console.ReadKey();
+        }
+    }
+}
